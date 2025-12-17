7353d506dd02976fc12c14621fdf9ebd2bd935a9
turkey1024 <ljx20110119@qq.com>
2025-12-17 21:50:17 +0800

    update

--- start.sh
+++ start.sh
@@ -4,18 +4,6 @@ set -e
 
 RENDER_PORT=${PORT:-1145}
 
-# 使用 structopt 的正确参数
-exec ./mk48-plus-bin \
-    --http-port "$RENDER_PORT" \
-    --debug-http info \
-    --debug-game info \
-    --debug-core info \
-    --debug-sockets warn \
-    --debug-watchdog info \
-    --http-bandwidth-limit 100000 \
-    --client-authenticate-burst 5 \
-    --server-id 1
-
 
 # 在 start.sh 中添加
 echo "服务器将监听端口: $PORT"
